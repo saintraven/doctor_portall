9436f9eef2302ed63b7092c026ef880d7570fc66
Artemiy Zhdankin <artyzh@inbox.ru>
2026-07-06 14:48:37 +0300

    entities created

---
+++ src/main/java/com/artyzh/model/Appointment.java
@@ -0,0 +1,30 @@
+package com.artyzh.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "appointments")
+@Getter
+@Setter
+public class Appointment {
+    @Id
+    private UUID id;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "doctor", nullable = false)
+    private Doctor doctor;
+    @Column(nullable = false)
+    private String patientName;
+    @Column(nullable = false)
+    private LocalDateTime appointmentDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    private String imageUrl;
+
+}
